6f279838a4dad79dc7953b0397a927be12b0a876
alelit4 <alelit4@gmail.com>
2014-09-08 19:37:52 +0200

    search and insert

--- src/com/qnoow/telephaty/bbdd/ControllerMensajes.java
+++ src/com/qnoow/telephaty/bbdd/ControllerMensajes.java
@@ -1,37 +1,47 @@
 package com.qnoow.telephaty.bbdd;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class ControllerMensajes {
-	String[] campos;
-	final SQLiteDatabase db;
+	SQLiteDatabase db;
 	BBDDMensajes mensajesDB;
+	String[] campos = new String[] { "id", "mac" };
 
 	public ControllerMensajes(Context context) {
 		mensajesDB = new BBDDMensajes(context, "Mensajes", null, 1);
-		db = mensajesDB.getWritableDatabase();
-		campos = new String[] { "id", "mac" };
 	}
 
 	public boolean insert(String id, String mac) {
-		String[] values = new String[] { id, mac };
-		Cursor cursor = db.query("Mensajes", campos, "id='" + id
-				+ "' AND mac='" + mac + "'", null, null, null, null);
-		if (cursor != null && !cursor.isClosed()) {
-			// TODO check the cursor 
-			if (!cursor.isFirst()) {
-				mensajesDB.insert(id, mac);
-				return true;
-			}
-			
+		db = mensajesDB.getWritableDatabase();
+		if (db != null && !search(id, mac)) {
+			db.execSQL("INSERT INTO Mensajes (id, mac) " + "VALUES ('" + id
+					+ "', '" + mac + "')");
+			db.close();
+			return true;
 		}
-		cursor.close();
 		return false;
 
 	}
+
+
+	private boolean search(String id, String mac) {
+
+		Cursor c = db.query("Mensajes", campos, null, null, null, null, null);
+
+		if (c.moveToFirst()) {
+			do {
+				if (c.getString(0).equals(id) && c.getString(1).equals(mac))
+					return true;
+			} while (c.moveToNext());
+		}
+		return false;
+	}
+
 }
